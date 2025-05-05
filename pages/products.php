5b5b66fd4fb1fc4c6f2bbe9a57dc612bafc569b0
jhon-mendez11 <jhon.danier2000@gmail.com>
2025-05-04 21:41:33 -0500

    Implementacióń de estilos y orden en la funcion de agregar productos

--- pages/products.php
+++ pages/products.php
@@ -4,30 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <title>Agregar Producto</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 
 <body>
-    <div class="contenedor">
+    <div class="p_contenedor">
         <h1>Agregar Producto</h1>
-        <form action="../server/products/add_products.php" method="POST">
-            <label>Nombre:</label>
-            <input type="text" name="nombre" required>
+        <form action="../server/products/add_products.php" method="POST" class="form-grid">
+            <div class="form-group">
+                <label>Nombre:</label>
+                <input type="text" name="nombre" required>
+            </div>
+            <div class="form-group">
+                <label>Cantidad:</label>
+                <input type="number" name="cantidad" min="0" required>
+            </div>
+            <div class="form-group">
+                <label>Precio (S/):</label>
+                <input type="number" step="0.01" name="precio" min="0" required>
+            </div>
+            <div class="form-group">
+                <label>Cantidad mínima:</label>
+                <input type="number" name="cantidad_min" min="0" required>
+            </div>
 
-            <label>Cantidad:</label>
-            <input type="number" name="cantidad" min="0" required>
-
-            <label>Precio (S/):</label>
-            <input type="number" step="00.01" name="precio" min="0" required>
-
-            <label>Cantidad mínima:</label>
-            <input type="number" name="cantidad_min" min="0" required>
-
-            <button type="submit">Guardar</button>
-            <a href="../index.php">Cancelar</a>
+            <div class="form-buttons">
+                <button type="submit">Guardar</button>
+                <a href="../index.php" class="cancel-btn">Cancelar</a>
+            </div>
         </form>
-        <a href="../index.php">volver</a>
+        <a href="../index.php" class="volver-link">Volver</a>
     </div>
+
 </body>
 
 </html>
